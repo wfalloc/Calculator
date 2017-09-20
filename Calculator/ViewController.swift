c825fddbd00ff9cab68882dacf2858804b834530
wuf <wuf@fengrongwang.com>
2017-09-20 11:04:34 +0800

    init project

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by klwx on 2017/9/20.
+//  Copyright © 2017年 wufan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    func setupUI() {
+        
+    }
+
+}
+
