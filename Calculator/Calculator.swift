cc212da3c84f27027d9f81533d1080f1f1a27866
wuf <wuf@fengrongwang.com>
2017-09-21 20:08:56 +0800

    commit

--- Calculator/Calculator.swift
+++ Calculator/Calculator.swift
@@ -8,6 +8,20 @@
 
 import UIKit
 
+// 只能进行整数运算
 class Calculator: NSObject {
-
+    
+    // 中缀表达式转换为后缀表达式
+    static func transformtTo(calCode: String) -> String {
+        let mathChars = ["+", "-", "*", "/", "(", ")"]
+        let stack = Stack()
+        var formuStr: [String] = []
+        
+        for mathMethod in mathChars {
+            formuStr = calCode.components(separatedBy: mathMethod)
+        }
+        
+        return formuStr
+    }
+    
 }
